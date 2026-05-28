34168bcbafc4e545919aa63959e49d3bd8665056
Mohanraj1414 <mohanrajrdj144@gmail.com>
2026-05-28 18:30:18 +0530

    Add FontAwesome icon definitions

---
+++ packages/icons_plus_fix/lib/src/font_awesome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// FontAwesome icon set
+class FontAwesome {
+  static const String _fontFamily = 'FontAwesome-Solids';
+
+  // File/import icons
+  static const IconData file_import_solid =
+      IconData(0xf56e, fontFamily: _fontFamily);
+
+  // Add more FontAwesome icons as needed
+}
